2059390a0aebfb9557ffef91f2be1aae8f1fa5c3
jakubhonisek <honisek.jakub@gmail.com>
2020-05-15 12:35:31 +0200

    Init

--- src/LaravelFormServiceProvider.php
+++ src/LaravelFormServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelFormServiceProvider extends ServiceProvider
 {
-    private $namespace = 'basics';
+    private $namespace = 'laravel-form';
 
     /**
      * Register services.
@@ -18,8 +18,12 @@ class LaravelFormServiceProvider extends ServiceProvider
         // config
         $this->mergeConfigFrom(__DIR__.'/../config/laravel-form.php', $this->namespace);
 
+        // views
+        $this->loadViewsFrom(__DIR__.'/views/', $this->namespace);
+
         // publishes
         $this->publishes([
+            __DIR__.'/views' => resource_path('views/vendor/'.$this->namespace),
             __DIR__.'/../config/laravel-form.php' => config_path($this->namespace.'.php'),
         ]);
     }
